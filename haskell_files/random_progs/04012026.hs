4cb9ab8181610a1e1c775cfc2c42422da009b5d9
src <src@org>
2026-04-01 22:03:34 -0400

    more haskell code, need to look at some haskell idioms

--- haskell_files/random_progs/04012026.hs
+++ haskell_files/random_progs/04012026.hs
@@ -8,16 +8,26 @@ data Pages = Pages [Page]
 data Book = Book BookTitle AuthorName NumberOfPages Pages
 data BookRow = BookRow [Book]
 
-lengthOfRow :: BookRow -> Int
-lengthOfRow [] = 0
-lengthOfRow (x:xs) = 1 + (lengthOfRow xs)
+-- lengthOfRow :: BookRow -> Int
+-- lengthOfRow [] = 0
+-- lengthOfRow (BookRow (x:xs)) = 1 + (lengthOfRow xs)
+
+filterEvenPageCount :: [Int] -> [Int]
+filterEvenPageCount [] = []
+filterEvenPageCount (x:xs) = 
+    if (x `mod` 2 == 0) 
+    then x:(filterEvenPageCount xs)
+    else (filterEvenPageCount xs)
 
 
-fold' :: (a -> b -> b) -> b -> [a] -> b
-fold' f acc [] = acc
-fold' f acc (x:xs) = (f x (fold' f )
+-- filterEvenPageCount' :: [Int] -> [Int]
+-- filterEvenPageCount' [] = []
+-- filterEvenPageCount (x:xs) = 
+-- fold' :: (a -> b -> b) -> b -> [a] -> b
+-- fold' f acc [] = acc
+-- fold' f acc (x:xs) = (f x (fold' f )
 
-lengthOfRow' :: BookRow -> Int
-lengthOfRow 
+-- lengthOfRow' :: BookRow -> Int
+-- lengthOfRow 
 
 
